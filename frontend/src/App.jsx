d393aece665247396fe2596dc2edd576cdda4aa4
maximiliano <max.borquezc@gmail.com>
2024-06-09 23:29:45 -0400

    cambio favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter, Route, Routes, useRoutes } from 'react-router-dom'

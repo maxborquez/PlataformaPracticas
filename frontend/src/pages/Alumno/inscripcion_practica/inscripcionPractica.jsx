4cfcd60df37aed22bb3f30579ffce88f1ec3ceb6
maximiliano <max.borquezc@gmail.com>
2024-06-16 10:18:55 -0400

    separacion de inscripcion de practica

--- frontend/src/pages/Alumno/inscripcion_practica/inscripcionPractica.jsx
+++ frontend/src/pages/Alumno/inscripcion_practica/inscripcionPractica.jsx
@@ -1,10 +1,21 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Alert } from "@mui/material";
 import Header from "../../../components/headers/header";
 import FormularioInscripcion from "./formularioInscripcion";
 import { School } from "@mui/icons-material";
 import SidebarAlumno from "../../../components/sidebars/sidebarAlumno";
+import clienteAxios from "../../../helpers/clienteaxios";
+import { useQuery } from "react-query";
 
 const InscripcionPractica = () => {
+  const id_alumno = localStorage.getItem("id_alumno");
+
+  const { data, status } = useQuery("estado_inscripcion", async () => {
+    const response = await clienteAxios.post("/inscripcion/comprobar", {
+      id_alumno: id_alumno,
+    });
+    return response.data;
+  });
+
   return (
     <Grid
       container
@@ -18,7 +29,16 @@ const InscripcionPractica = () => {
         <Header />
       </Grid>
 
-      <Grid item sx={{ zIndex: 999, position: "fixed", top: "80px", height: "calc(100vh - 80px)", overflowY: "auto" }}>
+      <Grid
+        item
+        sx={{
+          zIndex: 999,
+          position: "fixed",
+          top: "80px",
+          height: "calc(100vh - 80px)",
+          overflowY: "auto",
+        }}
+      >
         <SidebarAlumno />
       </Grid>
 
@@ -29,15 +49,37 @@ const InscripcionPractica = () => {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          marginLeft: "250px", 
+          marginLeft: "250px",
           width: "100%",
-          paddingTop: "80px", 
+          paddingTop: "80px",
         }}
       >
-        <Typography variant="h5" style={{ textAlign: "center", marginTop: "15px", marginBottom: "15px" }}>
-          Inscripción Práctica Profesional <School style={{ marginLeft: "5px" }} />
+        <Typography
+          variant="h5"
+          style={{
+            textAlign: "center",
+            marginTop: "15px",
+            marginBottom: "15px",
+          }}
+        >
+          Inscripción Práctica Profesional{" "}
+          <School style={{ marginLeft: "5px" }} />
         </Typography>
-        <FormularioInscripcion />
+        {status === "success" && data.inscrito_sistema && (
+          <Grid item>
+            {localStorage.setItem(
+              "id_inscripcion_practica",
+              data.id_inscripcion_practica
+            )}
+              <Alert severity="warning">Ya tienes una practica inscrita actualmente</Alert>
+          </Grid>
+        )}
+        {status === "success" && data.inscrito_sistema === false && (
+          <Grid item>
+            {localStorage.setItem("id_inscripcion_practica", "undefined")}
+            <FormularioInscripcion />
+          </Grid>
+        )}
       </Grid>
     </Grid>
   );

812c8ed6fd9f49f0783f103be1b0e4ad0d4d0904
EmersonLGR <114963787+EmersonLGR@users.noreply.github.com>
2024-06-25 20:03:31 -0400

    fix diseño paginas varias

--- frontend/src/pages/Alumno/inscripcion_practica/inscripcionPractica.jsx
+++ frontend/src/pages/Alumno/inscripcion_practica/inscripcionPractica.jsx
@@ -1,13 +1,20 @@
-import { Grid, Typography, Alert } from "@mui/material";
+import { Grid, Typography, Alert, Paper } from "@mui/material";
 import Header from "../../../components/headers/header";
 import FormularioInscripcion from "./formularioInscripcion";
 import { School } from "@mui/icons-material";
 import SidebarAlumno from "../../../components/sidebars/sidebarAlumno";
 import clienteAxios from "../../../helpers/clienteaxios";
 import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
 
 const InscripcionPractica = () => {
   const id_alumno = localStorage.getItem("id_alumno");
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
   const { data, status } = useQuery("estado_inscripcion", async () => {
     const response = await clienteAxios.post("/inscripcion/comprobar", {
@@ -16,70 +23,114 @@ const InscripcionPractica = () => {
     return response.data;
   });
 
+  useEffect(() => {
+    const handleResize = () => {
+      const zoomThreshold = 900;
+      setIsWideScreen(window.innerWidth >= zoomThreshold);
+      setSidebarOpen(window.innerWidth < zoomThreshold ? false : true);
+    };
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Grid
       container
-      sx={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-      }}
+      direction="column"
+      sx={{ backgroundColor: "#e8e9eb", minHeight: "100vh" }}
     >
-      <Grid item sx={{ zIndex: 1000, position: "fixed", width: "100%" }}>
-        <Header />
-      </Grid>
-
       <Grid
         item
-        sx={{
-          zIndex: 999,
-          position: "fixed",
-          top: "80px",
-          height: "calc(100vh - 80px)",
-          overflowY: "auto",
-        }}
+        sx={{ position: "sticky", top: 0, zIndex: 1000, width: "100%" }}
       >
-        <SidebarAlumno />
+        <Header
+          toggleSidebar={toggleSidebar}
+          isWideScreen={isWideScreen}
+          showSidebarButton={true}
+        />
       </Grid>
 
-      <Grid
-        item
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          marginLeft: "250px",
-          width: "100%",
-          paddingTop: "80px",
-        }}
-      >
-        <Typography
-          variant="h5"
-          style={{
-            textAlign: "center",
-            marginTop: "15px",
-            marginBottom: "15px",
+      <Grid container item>
+        {sidebarOpen && (
+          <Grid
+            item
+            sx={{
+              position: "fixed",
+              top: "80px",
+              left: 0,
+              width: "250px",
+              zIndex: 1200,
+              backgroundColor: "#36465d",
+              height: "calc(100vh - 80px)", // Ajustar altura para ocupar todo el espacio disponible
+            }}
+          >
+            <SidebarAlumno />
+          </Grid>
+        )}
+
+        <Grid
+          item
+          xs
+          sx={{
+            marginLeft: sidebarOpen && isWideScreen ? "250px" : "0px",
+            transition: "margin-left 0.3s",
+            overflowY: "auto",
+            paddingRight: "16px",
+            paddingLeft: "16px",
+            paddingTop: "16px",
+            paddingBottom: "16px",
+            display: "flex",
+            justifyContent: "center",
+            width: "auto", // Asegurar que el Grid ocupe todo el ancho disponible
           }}
         >
-          Inscripción Práctica Profesional{" "}
-          <School style={{ marginLeft: "5px" }} />
-        </Typography>
-        {status === "success" && data.inscrito_sistema && (
-          <Grid item>
-            {localStorage.setItem(
-              "id_inscripcion_practica",
-              data.id_inscripcion_practica
+          <Paper
+          elevation={3}
+            sx={{
+              padding: "20px",
+              backgroundColor: "white",
+              width: "100%",
+              // Máximo ancho para hacerlo más responsivo
+
+              borderRadius: "8px", // Bordes redondeados
+              boxSizing: "border-box", // Asegurar que el padding no cause overflow
+            }}
+          >
+            <Typography
+              variant="h5"
+              style={{
+                textAlign: "center",
+                marginTop: "15px",
+                marginBottom: "15px",
+              }}
+            >
+              Inscripción Práctica Profesional{" "}
+              <School style={{ marginLeft: "5px" }} />
+            </Typography>
+            {status === "success" && data.inscrito_sistema && (
+              <Grid item>
+                {localStorage.setItem(
+                  "id_inscripcion_practica",
+                  data.id_inscripcion_practica
+                )}
+                <Alert severity="warning">
+                  Ya tienes una practica inscrita actualmente
+                </Alert>
+              </Grid>
             )}
-              <Alert severity="warning">Ya tienes una practica inscrita actualmente</Alert>
-          </Grid>
-        )}
-        {status === "success" && data.inscrito_sistema === false && (
-          <Grid item>
-            {localStorage.setItem("id_inscripcion_practica", "undefined")}
-            <FormularioInscripcion />
-          </Grid>
-        )}
+            {status === "success" && data.inscrito_sistema === false && (
+              <Grid item>
+                {localStorage.setItem("id_inscripcion_practica", "undefined")}
+                <FormularioInscripcion />
+              </Grid>
+            )}
+          </Paper>
+        </Grid>
       </Grid>
     </Grid>
   );

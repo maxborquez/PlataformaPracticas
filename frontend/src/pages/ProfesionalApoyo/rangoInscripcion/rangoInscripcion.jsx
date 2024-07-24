cc3efd07732e730cb89ea8083706e6fbb845701c
maximiliano <max.borquezc@gmail.com>
2024-07-24 17:46:49 -0400

    page rango de inscripcion

---
+++ frontend/src/pages/ProfesionalApoyo/rangoInscripcion/rangoInscripcion.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+import { Grid, Card } from "@mui/material";
+import Header from "../../../components/headers/header";
+import SidebarProfesional from "../../../components/sidebars/sidebarProfesional";
+import { useNavigate } from "react-router-dom";
+
+
+const RangoInscripcion = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const zoomThreshold = 900;
+      setIsWideScreen(window.innerWidth >= zoomThreshold);
+      setSidebarOpen(window.innerWidth < zoomThreshold ? false : true);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Grid
+      container
+      direction="column"
+      sx={{ backgroundColor: "#e8e9eb", minHeight: "100vh" }}
+    >
+      <Grid
+        item
+        sx={{ position: "sticky", top: 0, zIndex: 1000, width: "100%" }}
+      >
+        <Header
+          toggleSidebar={toggleSidebar}
+          isWideScreen={isWideScreen}
+          showSidebarButton={true}
+        />
+      </Grid>
+
+      <Grid container item>
+        {sidebarOpen && (
+          <Grid
+            item
+            sx={{
+              position: "fixed",
+              top: "80px",
+              left: 0,
+              width: "250px",
+              zIndex: 1200,
+              backgroundColor: "#36465d",
+            }}
+          >
+            <SidebarProfesional />
+          </Grid>
+        )}
+
+        <Grid
+          item
+          xs
+          sx={{
+            marginLeft: sidebarOpen && isWideScreen ? "250px" : "0px",
+            transition: "margin-left 0.3s",
+            overflowY: "auto",
+            paddingRight: "16px",
+            overflowX: "auto",
+            marginTop: "16px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Card
+            sx={{
+              padding: "20px",
+              backgroundColor: "white",
+              width: "100%",
+              marginBottom: "15px",
+              marginLeft: "16px",
+            }}
+          >
+           <h1>Lorem</h1>
+          </Card>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default RangoInscripcion;

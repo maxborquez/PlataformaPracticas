ab7d44d17bef98271f396144ad3afe8330b117f1
maximiliano <max.borquezc@gmail.com>
2024-07-31 04:30:41 -0400

    request sp funcionales

--- backend/src/StorageProcedures/executeSp.js
+++ backend/src/StorageProcedures/executeSp.js
@@ -4,9 +4,16 @@ const config = require('../../dbConfig');
 const executeStoredProcedure = async (procedureName, params) => {
   try {
     let pool = await sql.connect(config);
-    let result = await pool.request()
-      .input('alu_rut', sql.Int, params.alu_rut) // asegúrate de que el nombre del parámetro coincida
-      .execute(procedureName);
+    let request = pool.request();
+    
+    // Verifica y agrega los parámetros necesarios
+    for (const [key, value] of Object.entries(params)) {
+      if (value && value.type !== undefined) {
+        request.input(key, value.type, value.value);
+      }
+    }
+    
+    let result = await request.execute(procedureName);
 
     return result.recordset;
   } catch (err) {
--- backend/src/routes/spRoutes.js
+++ backend/src/routes/spRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getDatosAlumno } = require('../controllers/spcontroller');
+const { getDatosAlumno, getAlumnosPorAsignaturaPractica, getConveniosVigentes, getIdentificacionPractica } = require('../controllers/spcontroller');
 
-router.get('/datos-alumno/:id', getDatosAlumno);
+router.get('/datosAlumno/:alu_rut', getDatosAlumno);
+router.get('/alumnosPractica/:ano/:periodo', getAlumnosPorAsignaturaPractica);
+router.get('/convenios/:fecha_inicio', getConveniosVigentes);
+router.get('/auth/:n_rut/:password', getIdentificacionPractica);
 
 module.exports = router;
